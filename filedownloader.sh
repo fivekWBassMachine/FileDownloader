e9a17eb5439e94d993cc92ce7cc700ae18e177f3
fivekWBassMachine <53333237+fivekWBassMachine@users.noreply.github.com>
2020-05-30 20:34:46 +0200

    [FEAT-ADD] md5sum verification; [BUG-FIX] min/maj
    rewrite of whole script;
    - added md5sum verification of each file (except the listfile)
    - commented out some unused color codes
    - minor improvements
    - the logfile has the same name as the bash script
    - bugfixes

--- filedownloader.sh
+++ filedownloader.sh
@@ -21,46 +21,51 @@
 #  
 #  
 
+# some useful foreground color codes
+# some are commented out due to performance reasons...
 COL_CLEAR='\e[0m'
-COL_BLACK='\e[0;30m'
+#COL_BLACK='\e[0;30m'
 COL_RED='\e[0;31m'
 COL_GREEN='\e[0;32m'
-COL_BROWN='\e[0;33m'
+#COL_BROWN='\e[0;33m'
 COL_BLUE='\e[0;34m'
-COL_PURPLE='\e[0;35m'
-COL_CYAN='\e[0;36m'
+#COL_PURPLE='\e[0;35m'
+#COL_CYAN='\e[0;36m'
 COL_LGRAY='\e[0;37m'
 COL_DGRAY='\e[1;30m'
-COL_LRED='\e[1;31m'
-COL_LGREEN='\e[1;32m'
-COL_YELLOW='\e[1;33m'
-COL_LBLUE='\e[1;34m'
-COL_LPURPLE='\e[1;35m'
-COL_LCYAN='\e[1;36m'
+#COL_LRED='\e[1;31m'
+#COL_LGREEN='\e[1;32m'
+#COL_YELLOW='\e[1;33m'
+#COL_LBLUE='\e[1;34m'
+#COL_LPURPLE='\e[1;35m'
+#COL_LCYAN='\e[1;36m'
 COL_WHITE='\e[1;37m'
 
 # return values:
 #  0: success
 #  1: failed
+#  2: precondition failed
 
+# log messages
 function info() {
 	d=`date '+%FT%T:%2N'`
 	echo -e "${COL_WHITE}[${COL_BLUE}i${COL_WHITE}]${COL_LGRAY} [${d}]${COL_LGRAY} ${1}${COL_DGRAY}"
-	echo "[i] [${d}] ${1}" >> $working_dir/filedownloader.log
+	echo "[i] [${d}] ${1}" >> $logfile
 }
 function error() {
 	d=`date '+%FT%T:%2N'`
 	echo -e "${COL_WHITE}[${COL_RED}✗${COL_WHITE}]${COL_LGRAY} [${d}]${COL_LGRAY} ${1}${COL_DGRAY}"
-	echo "[✗] [${d}] ${1}" >> $working_dir/filedownloader.log
+	echo "[✗] [${d}] ${1}" >> $logfile
 }
 function success() {
 	d=`date '+%FT%T:%2N'`
 	echo -e "${COL_WHITE}[${COL_GREEN}✓${COL_WHITE}]${COL_LGRAY} [${d}]${COL_LGRAY} ${1}${COL_DGRAY}"
-	echo "[✓] [${d}] ${1}" >> $working_dir/filedownloader.log
+	echo "[✓] [${d}] ${1}" >> $logfile
 }
 
+# display usage of this script
 function show_usage() {
-	echo "filedownloader.sh copyright 5kWBassMachine 2020"
+	echo "$filename copyright 5kWBassMachine 2020"
 	echo "ARGUMENTS:"
 	echo " \$1: url to the files repository (*.filelist)"
 	echo " \$2: path to the download directory"
@@ -74,53 +79,172 @@ function show_usage() {
 	echo " https://www.gnu.org/licenses/gpl-3.0.txt"
 }
 
+# creates dir $1 if not existent
 function create_dir() {
 	if [ ! -d $1 ]; then
 		info "Creating directory '$1'..."
 		mkdir -p $1
 		if [ $? -eq 0 ]; then
 			success "Done."
+			return 0
 		else
 			error "Can't create directory."
-			exit 1
+			return 1
 		fi
 	fi
 }
 
+# removes dir $1 if eistent
+function remove_dir() {
+	if [ -d $1 ]; then
+		info "Removing directory '$1'..."
+		rm -r $1
+		if [ $? -eq 0 ]; then
+			success "Done."
+			return 0
+		else
+			error "Can't remove directory."
+			return 1
+		fi
+	fi
+}
+
+# move all files in dir $1 to dir $2
+function move_files_in_dir() {
+	if [ -d $1 ] && [ -d $2 ]; then
+		src=$1*; if [[ "$1" != */ ]]; then src=$1/*; fi
+		info "Moving '$src' to '$2'..."
+		mv $src $2
+		if [ $? -eq 0 ]; then
+			success "Done."
+			return 0
+		else
+			error "Can't move files."
+			return 1
+		fi
+	else
+		error "Can't move files. (the source or target directory isn't valid)"
+		return 2
+	fi
+}
+
+# downloads a file $1 using wget
 function download_file() {
 	info "Downloading file '$1'..."
 	wget $1
 	if [ $? -eq 0 ]; then
 		success "Done."
+		return 0
 	else
 		error "Can't download file."
-		exit 1
+		return 1
 	fi
 }
 
+# proves wether a file $2 has the same checksum as $1 using md5sum
+function check_checksum() {
+	if [ -z $1 ]; then
+		error "Empty checksum."
+		return 1
+	fi
+	if [ ! -e $2 ]; then
+		error "No such file: $2"
+		return 1
+	fi
+	info "Getting md5 checksum of '$2'..."
+	IFS="  " read -ra checksum <<< $(md5sum $2)
+	if [ ${checksum[0]} = $1 ]; then
+		success "Done. Checksums match."
+		return 0
+	else
+		error "Done. Checksums don't match."
+		return 1
+	fi
+}
+
+# decodes url encodings to characters (e.g. '%2B' to '+')
+function urldecode() { : "${*/// }"; echo -e "${_//%/\\x}"; }
+
+# replaces ' ' with '\ '
+function escape_spaces() { echo ${1//" "/"\\ "}; }
+
+# removes all temporary files and dirs and exits with $1
+function clean_exit() {
+	remove_dir $tmp_dir
+	info "Script exited with code $1$COL_CLEAR"
+	exit $1
+}
+
+filename=$0
+
 if [ -z $2 ]; then
+	# show usage when less than 2 arguments are given.
 	show_usage
 	exit 2
 else
+	# init
 	working_dir=$(pwd)
+	tmp_dir="$working_dir/tmp_`date '+%FT%H-%M-%S'`"
 	
-	create_dir $2
+	# get logdir
+	IFS='/' read -ra logfile <<< "$filename"
+	IFS='.' read -ra logfile <<< "${logfile[1]}"
+	logfile=$working_dir/${logfile[0]}.log
+	
+	# start real script execution
+	
+	# create temporary dir & cd to it
+	create_dir $tmp_dir
 	if [ $? -ne 0 ]; then exit 1; fi
 	
-	cd $2
-	if [ $? -ne 0 ]; then exit 1; fi
+	cd $tmp_dir
+	if [ $? -ne 0 ]; then error "Can't cd to '$tmp_dir'"; clean_exit 1; fi
 	
+	# get filelist
 	info "Downloading filelist..."
-	file_list="`wget -O- $1`"
-	if [ $? -ne 0 ]; then exit 1; else success "Done."; fi
+	filelist="`wget -O- $1`"
+	if [ $? -ne 0 ]; then error "Can't download filelist"; clean_exit 1; else success "Done."; fi
 	
-	for file_url in $file_list; do
-		if [[ $file_url == \#* ]]; then
+	# iterate through each entry in filelist
+	for line in $filelist; do
+		if [[ $line == \#* ]]; then
+			# skip entries starting with a '#'
 			info "Skipping comment."
 		else
-			IFS='/' read -ra file_url_parts <<< "$file_url"
-			file_name=${file_url_parts[-1]}
-			download_file $file_url $file_name
+			# get checksum, url and name
+			IFS='|' read -ra line_parts <<< "$line"
+			checksum=${line_parts[0]}
+			url=${line_parts[1]}
+			IFS='/' read -ra url_parts <<< "$url"
+			name=${url_parts[-1]}
+			name=$(urldecode $name)
+			echo $name
+			
+			# download the entry
+			download_file $url $name
+			if [ $? -ne 0 ]; then clean_exit 1; fi
+			
+			echo $name
+			# prove the entry
+			check_checksum $checksum $name
+			if [ $? -ne 0 ]; then clean_exit 1; fi
 		fi
 	done
+	info "All files downloaded and validated."
+	
+	cd $working_dir
+	
+	# create target dir if not existent
+	create_dir $2
+	if [ $? -ne 0 ]; then clean_exit 1; fi
+	move_files_in_dir $tmp_dir $2
+	if [ $? -ne 0 ]; then clean_exit 1; fi
+	
+	# success message
+	entries=( $filelist )
+	success "${COL_GREEN}Successfully downloaded and verified ${#entries[@]} files."
+	info "The files are in '$2'"
+	
+	# exit programm
+	clean_exit 0
 fi
